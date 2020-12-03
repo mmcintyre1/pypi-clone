38153ce106f694be79215ae599b390b2029742d9
Mike <mfmcintyre01@gmail.com>
2020-12-02 22:15:14 -0500

    updated structure of app.py

--- pypi_org/app.py
+++ pypi_org/app.py
@@ -1,36 +1,55 @@
-import flask
-import pathlib
+import os
 import sys
 
-folder = pathlib.Path(__file__).parent.parent
+import flask
+
+folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, folder)
 import pypi_org.data.db_session as db_session
-
 
 app = flask.Flask(__name__)
 
 
 def main():
-    register_blueprints()
-    setup_db()
+    configure()
     app.run(debug=True, port=5006)
 
 
+def configure():
+    print("Configuring Flask app:")
+
+    register_blueprints()
+    print("Registered blueprints")
+
+    setup_db()
+    print("DB setup completed.")
+    print("", flush=True)
+
+
 def setup_db():
-    db_file = pathlib.Path(__file__).parent / 'db' / 'pypi_org.sqlite'
-    db_session.global_init(str(db_file))
+    db_file = os.path.join(
+        os.path.dirname(__file__),
+        'db',
+        'pypi.sqlite')
+
+    db_session.global_init(db_file)
 
 
 def register_blueprints():
     from pypi_org.views import home_views
-    from pypi_org.views import account_views
     from pypi_org.views import package_views
     from pypi_org.views import cms_views
-    app.register_blueprint(blueprint=home_views.blueprint)
-    app.register_blueprint(blueprint=package_views.blueprint)
-    app.register_blueprint(blueprint=cms_views.blueprint)
-    app.register_blueprint(blueprint=account_views.blueprint)
+    from pypi_org.views import account_views
+    from pypi_org.views import seo_view
+
+    app.register_blueprint(package_views.blueprint)
+    app.register_blueprint(home_views.blueprint)
+    app.register_blueprint(account_views.blueprint)
+    app.register_blueprint(seo_view.blueprint)
+    app.register_blueprint(cms_views.blueprint)
 
 
 if __name__ == '__main__':
     main()
+else:
+    configure()
